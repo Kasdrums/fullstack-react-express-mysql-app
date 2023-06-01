8b23b6ffe11daa19efd8f10af9a26fb272a54a98
Alexander <thelegindrum@gmail.com>
2023-06-01 20:58:43 +0300

    add commit

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ const express = require('express')
 
 const app = express()
 
+const bricks = []
+
+app.get('api/bricks', (req, res) => {
+	console.log('Get all bricks')
+	res.send('OK')
+})
+
+app.post('/api/bricks', (req, res) => {
+	console.log('Add new brick')
+	res.send('OK')
+})
+
 app.listen(8000, () => {
 	console.log('Server is running')
 })
