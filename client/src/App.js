f331ae281689a473cd4b3b235271434b3645ade0
Alexander <thelegindrum@gmail.com>
2023-06-01 18:47:02 +0300

    add commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
-function App() {
-	return (
-		<div className="App">
-			<h1>The App</h1>
-		</div>
-	)
+import React from 'react'
+class App extends React.Component {
+	render() {
+		return (
+			<div className="App">
+				<h1>The App</h1>
+			</div>
+		)
+	}
 }
 
 export default App
