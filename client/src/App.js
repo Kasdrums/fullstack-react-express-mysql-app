c7ef8895dcc6909a514759207855c3a9ee98fbec
Alexander <thelegindrum@gmail.com>
2023-06-01 20:52:33 +0300

    add components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Form from './components/Form'
+import List from './components/List'
 class App extends React.Component {
 	render() {
 		return (
 			<div className="App">
 				<h1>The App</h1>
+				<Form />
+				<List />
 			</div>
 		)
 	}
---
+++ client/src/components/Form.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Form extends React.Component {
+	render() {
+		return (
+			<div className="App">
+				<input type="text " />
+				<button>Send message</button>
+			</div>
+		)
+	}
+}
+
+export default Form
